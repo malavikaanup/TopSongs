af5ebab1a3c3857614bc2b080265f682f7e33bd8
Malavika Akhilesh <malavikaakhilesh@Malavikas-MBP-2.cg.shawcable.net>
2024-03-29 16:36:49 -0600

    New branch

---
+++ TopSongs/ViewController/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  TopSongs
+//
+//  Created by Malavika Akhilesh on 29/03/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TopSongs/ViewController/PlayerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PlayerViewController.swift
+//  TopSongs
+//
+//  Created by Malavika Akhilesh on 29/03/24.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        self.viewDidLoad()
+    }
+}
